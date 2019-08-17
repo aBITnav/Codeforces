9b7237b9f35ffddfc7ed6fe9717b000eef746312
Abhinav <abhinav98jha@gmail.com>
2019-08-17 15:47:00 +0530

    Add solution for problem `B - Buttons` Link: https://codeforces.com/contest/268/problem/B Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/268/submission/58949265

--- codeforces/268/B.cpp
+++ codeforces/268/B.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-int n;
-cin>>n;
-int i;
-long long s=0;
-for(i=0;i<n;i++)
-s+=(n-i)*i+1;
-cout<<s;
-return 0;
 
+int main() {
+	int n;
+	cin>>n;
+	int a[n+1];
+	a[0]=0;a[1]=1;a[2]=3;
+	for(int i=3;i<=n;i++)
+	a[i]=a[i-1]+(i*(i-1))/2 + 1;
+	cout<<a[n];
+	return 0;
 }
