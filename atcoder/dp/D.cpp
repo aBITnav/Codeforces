ec619044bf8050565b334b46237ecc99f0f809f2
Abhinav <abhinav98jha@gmail.com>
2019-12-05 01:03:00 +0530

    Add solution for problem `D - Knapsack 1` Link: https://atcoder.jp/contests/dp/tasks/dp_d Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/8792474

--- atcoder/dp/D.cpp
+++ atcoder/dp/D.cpp
@@ -1,25 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define int long long
-#define rep(i,n) for(i=0;i<n;i++)
-#define pb push_back
-#define all(a) a.begin(),a.end()
-#define fi first
-#define se second
-int M=1e9+7;
-int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
-int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
 
-signed main(){
-ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-	int n,s,mx=0;
+signed main() {
+	int n,s;
 	cin>>n>>s;
-	int w[n],dp[s+1]={},v[n];
-	for(int i=0;i<n;i++) cin>>w[i]>>v[i];
-	for(int i=0;i<n;i++)
-		for(int j=s;j>=w[i];j--) 
-			dp[j]=max(dp[j-1],dp[j-w[i]]+v[i]);
-	//cout<<dp[s];
-	for(int i:dp) mx=max(mx,i);
-	cout<<mx;
+	int w[n+1],v[n+1];
+	for(int i=1;i<=n;i++) cin>>w[i]>>v[i];
+	int dp[n+1][s+1];
+	for(int i=0;i<=n;i++)
+		for(int j=0;j<=s;j++)
+			if(i==0 or j==0) dp[i][j]=0;
+			else if(j>=w[i])
+			dp[i][j]=max(dp[i-1][j],dp[i-1][j-w[i]]+v[i]);
+			else dp[i][j]=dp[i-1][j];
+	cout<<dp[n][s];	
+	return 0;
 }
