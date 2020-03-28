c573bc8a8e6387e0a9871370cd4b15d78c36e985
Abhinav <abhinav98jha@gmail.com>
2020-03-28 18:41:00 +0530

    Add solution for problem `C - Traveling Salesman around Lake` Link: https://atcoder.jp/contests/abc160/tasks/abc160_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc160/submissions/11305617

---
+++ atcoder/abc160/C.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define all(a) (a).begin(),(a).end()
+#define fi first
+#define se second
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int M=1e9+7;
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+signed main(){
+	fast;
+	string s;
+	vector<int> v;
+	map<int,int> mp;
+	int n,m,c=0,t=1;
+	//cin>>t;
+	while(t--){
+		cin>>m>>n;
+		int a[n],dmax=0,am=0;
+		for(int i=0;i<n;i++) {
+			cin>>a[i];
+		}
+		sort(a,a+n);
+		for(int i=1;i<n;i++){
+			dmax=max(dmax,a[i]-a[i-1]);
+		}
+		dmax=max(dmax,m-a[n-1]+a[0]);
+		cout<<m-dmax;
+
+	}
+	return 0;
+}
+
