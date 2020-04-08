fd36368ffee007e76632be9e34a13ecc0bf7cde3
Abhinav <abhinav98jha@gmail.com>
2020-04-08 20:54:00 +0530

    Add solution for problem `B - Kind Anton` Link: https://codeforces.com/contest/1333/problem/B Tags: greedy, implementation, *1100 Ref: https://codeforces.com/contest/1333/submission/75878174

---
+++ codeforces/1333/B.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define all(a) (a).begin(),(a).end()
+#define fi first
+#define se second
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int M=1e9+7;
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+signed main(){
+	fast;
+	//string s;
+	int n,m,c=0,t=1;
+	cin>>t;
+	while(t--){
+		cin>>n;
+		int a[n],b[n];
+		set<int> s;
+		for(int i=0;i<n;i++) cin>>a[i];
+		for(int i=0;i<n;i++) cin>>b[i];
+		string ans="YES";
+		for(int i=0;i<n;i++){
+			if(b[i]>a[i] and s.find(1)==s.end()) ans="NO"; 
+			else if(b[i]<a[i] and s.find(-1)==s.end()) ans="NO"; 
+			s.insert(a[i]);
+			}
+		cout<<ans<<endl;
+	}
+	
+
+	return 0;
+}
+
