42ce8d1f17e7eb7f44a222f60a8c2a9cb0e44cbb
Abhinav <abhinav98jha@gmail.com>
2020-04-19 17:35:00 +0530

    Add solution for problem `A - Circle Pond` Link: https://atcoder.jp/contests/abc163/tasks/abc163_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc163/submissions/12087726

--- atcoder/abc163/A.cpp
+++ atcoder/abc163/A.cpp
@@ -12,7 +12,9 @@ int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
 int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
 
 signed main(){
-
+#ifndef ONLINE_JUDGE 
+freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+#endif
 ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 	//string s;
 	int n,m,c=0,t=1,i,j,k,sum=0,ans,f=0,x,y;
