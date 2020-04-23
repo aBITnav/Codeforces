44cbcd4282aa8d2eb4e8fc779ca72598a3caafee
Abhinav <abhinav98jha@gmail.com>
2020-04-23 20:34:00 +0530

    Add solution for problem `A - Nastya and Rice` Link: https://codeforces.com/contest/1341/problem/A Tags: math, *900 Ref: https://codeforces.com/contest/1341/submission/77787499

---
+++ codeforces/1341/A.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define rep(i,n) for(i=0;i<n;i++)
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+int M=1e9+7;
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	
+	int n,m,c=0,t=1,i,j,k,sum=0,ans,f=0,x,y,a,b,d,h,l,nh,nl,rl,rh;
+	cin>>t;
+	while(t--){
+		string s="Yes";
+		//vector<int> v;map<int,int> mp;set<int> st;
+		cin>>n>>a>>b>>c>>d;
+	    nh=a+b;
+	    nl=a-b;
+	    rl=n*nl;
+	    rh=n*nh;
+	    if(rl>c+d || rh<c-d)
+		s="No"; 
+		cout<<s<<endl;
+		//int a[n];rep(i,n) cin>>a[i];
+
+	}
+	return 0;
+}
+
+
