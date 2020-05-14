fff833481ff5c86a3dca1434556914428274effd
Abhinav <abhinav98jha@gmail.com>
2020-05-14 20:17:00 +0530

    Add solution for problem `A - Most Unstable Array` Link: https://codeforces.com/contest/1353/problem/A Tags: constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1353/submission/80090522

---
+++ codeforces/1353/A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define rep(i,n) for(i=0;i<n;i++)
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+int M=1e9+7;
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	//string s;
+	int n,m,c=0,t=1,i,j,k,sum=0,f=0,x,y;
+	cin>>t;
+	while(t--){
+		//vector<int> v;map<int,int> mp;set<int> st;
+		cin>>n>>m;
+		int ans=0;
+		if(n==1) ans=0;
+		else if(n==2) ans=m;
+		else ans=2*m;
+		cout<<ans<<endl;
+		
+
+	}
+	return 0;
+}
+
+
