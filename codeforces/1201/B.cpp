eac346de526053e243c72612f81d0bdc856dd044
Abhinav <abhinav98jha@gmail.com>
2020-05-31 12:54:00 +0530

    Add solution for problem `B - Zero Array` Link: https://codeforces.com/contest/1201/problem/B Tags: greedy, math, *1500 Ref: https://codeforces.com/contest/1201/submission/82019068

---
+++ codeforces/1201/B.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define rep(i,n) for(i=0;i<n;i++)
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+int M=1e9+7;
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++) cin>>a[i];
+	int s=0;
+	for(int i:a) s+=i;
+	string ans="YES\n";
+	if(s%2) ans="NO\n";
+	for(int i:a) if(i>s/2) ans="NO\n";
+	cout<<ans;
+}
+
+
