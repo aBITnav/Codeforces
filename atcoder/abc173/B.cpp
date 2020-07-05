4c353ebc58e9ce7f5073e6fc56d7842b33e3b4b1
Abhinav <abhinav98jha@gmail.com>
2020-07-05 17:41:00 +0530

    Add solution for problem `B - Judge Status Summary` Link: https://atcoder.jp/contests/abc173/tasks/abc173_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc173/submissions/14981351

---
+++ atcoder/abc173/B.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+#define deb(x) cout << #x << "=" << x << endl
+#define clr(x) memset(x, 0, sizeof(x))
+#define rep(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+int M=1e9+7,N=205001;
+
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+
+
+void solve() {
+	int n;
+	cin>>n;
+	int ac=0,wa=0,tle=0,re=0;
+	string s;
+	while(n--){
+		cin>>s;
+		if(s=="AC") ac++;
+		else if(s=="WA") wa++;
+		else if(s=="TLE") tle++;
+		else re++;
+	}
+	cout<<"AC x "<<ac<<"\nWA x "<<wa<<"\nTLE x "<<tle<<"\nRE x "<<re;
+	
+}
+
+
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int t=1;
+	//cin>>t;
+	while(t--) solve();
+	return 0;
+}
+
+
+
