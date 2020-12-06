edad10f564ab157ca85752eb746ba7651c12d58c
Abhinav <abhinav98jha@gmail.com>
2020-12-06 13:39:00 +0530

    Add solution for problem `B - Filling the Grid` Link: https://codeforces.com/contest/1228/problem/B Tags: implementation, math, *1400 Ref: https://codeforces.com/contest/1228/submission/100501463

---
+++ codeforces/1228/B.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+#define deb(x) cout << #x << "=" << x << endl
+#define clr(x) memset(x, 0, sizeof(x))
+#define rep(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+int M=1e9+7,N=205001;
+
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+//1:full
+//2:empty
+
+void solve() {
+	int m,n,x,c=0;
+	cin>>m>>n;
+	int g[m][n];
+	memset(g,0,sizeof(g));
+	rep(i,0,m){
+		cin>>x;
+		rep(j,0,x) g[i][j]=1;
+		if(x<n) g[i][x]=2;
+	}
+	rep(i,0,n){
+		cin>>x;
+		rep(j,0,x) 
+			if(g[j][i]==2){
+				cout<<0;
+				return;
+			}
+			else g[j][i]=1;
+		if(x<m) {
+			if(g[x][i]==1){
+				cout<<0;
+				return;
+			}
+			g[x][i]=2;
+		}
+	}
+	rep(i,0,m) rep(j,0,n) c+=(g[i][j]==0);
+	cout<<po(2,c);
+}
+
+
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int t=1;
+	//cin>>t;
+	while(t--) solve();
+	return 0;
+}
+
+
+
