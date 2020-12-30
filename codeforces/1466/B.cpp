dc228029a6b44af62061349a82da8bc6464c37e0
Abhinav <abhinav98jha@gmail.com>
2020-12-30 20:32:00 +0530

    Add solution for problem `B - Last minute enhancements` Link: https://codeforces.com/contest/1466/problem/B Tags: dp, greedy, *800 Ref: https://codeforces.com/contest/1466/submission/102798110

---
+++ codeforces/1466/B.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+#define deb(x) cout << #x << "=" << x << endl
+#define clr(x) memset(x, 0, sizeof(x))
+#define rep(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+int M=1e9+7,N=205001;
+
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+
+
+void solve() {
+	int n,x,p=0;
+	cin>>n;
+	int m[2*n+2]={};
+	rep(i,0,n){
+		cin>>x;
+		m[x]++;
+	}
+	rep(i,1,2*n+2){
+		if(m[i]>1) m[i+1]++;
+	}
+	int ans=0;
+	rep(i,1,2*n+2){
+		if(m[i]>=1) ans++;
+	}
+	cout<<ans<<"\n";
+}
+
+
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int t=1;
+	cin>>t;
+	while(t--) solve();
+	return 0;
+}
+
+
+
