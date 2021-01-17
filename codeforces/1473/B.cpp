ccd9bcbae5d3c75d3f6a383e73c686ca987591f5
Abhinav <abhinav98jha@gmail.com>
2021-01-17 19:59:00 +0530

    Add solution for problem `B - String LCM` Link: https://codeforces.com/contest/1473/problem/B Tags: brute force, math, number theory, strings, *1000 Ref: https://codeforces.com/contest/1473/submission/104593655

---
+++ codeforces/1473/B.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+#define deb(x) cout << #x << "=" << x << endl
+#define clr(x) memset(x, 0, sizeof(x))
+#define rep(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+int M=1e9+7,N=205001;
+ 
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+ 
+int per(string s){
+	int n=s.size();
+	rep(i,1,n+1){
+		if(n%i) continue;
+		int f=1;
+		rep(j,i,n) if(s[j%i]!=s[j]){
+			f=0;break;
+		}
+		if(f) return i;
+	}
+	return n;
+} 
+
+void solve() {
+	string s,t;
+	cin>>s>>t;
+	int m=per(s),n=per(t);
+	if(s.substr(0,m)!=t.substr(0,n)) {
+		cout<<"-1\n";
+		return;
+	}
+	string p=s.substr(0,m);
+	m=s.size()/m;
+	n=t.size()/n;
+	m=(m*n)/__gcd(m,n);
+	string ans="";
+	while(m--) ans+=p;
+	cout<<ans<<"\n";
+}
+ 
+ 
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int t=1;
+	cin>>t;
+	while(t--) solve();
+	return 0;
+}
+ 
+ 
