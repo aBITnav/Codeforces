0c16dcaa9972f36652ed6946bca006c2ae262ac2
Abhinav <abhinav98jha@gmail.com>
2021-02-04 21:38:00 +0530

    Add solution for problem `D - Colorful Graph` Link: https://codeforces.com/contest/246/problem/D Tags: brute force, dfs and similar, graphs, *1600 Ref: https://codeforces.com/contest/246/submission/106472537

--- codeforces/246/D.cpp
+++ codeforces/246/D.cpp
@@ -13,24 +13,33 @@ const int M=1e9+7,N=205001;
 int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
 int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
 
+vector<int> g[N],vis(N),c(N);
+map<int,set<int>> mp;
+
+void dfs(int s){
+	vis[s]=1;
+	for(int i:g[s]){
+		if(!vis[i]) dfs(i);
+		if(c[i]!=c[s])	mp[c[s]].insert(c[i]);
+	}
+}
+
 
 signed main(){
 ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 	int n,m,x,y,ans=0,mx=0;
 	cin>>n>>m;
-	vector<int> g[n+1],vis(n+1),c(n+1);
-	map<int,set<int>> mp;
 	rep(i,1,n+1) cin>>c[i];
 	while(m--){
 		cin>>x>>y;
 		g[x].pb(y);
 		g[y].pb(x);
 	}
-	rep(s,1,n+1) for(int i:g[s])
-		if(c[i]!=c[s])	mp[c[s]].insert(c[i]);
+	rep(i,1,n+1) if(!vis[i]) dfs(i);
 	sort(c.begin()+1,c.begin()+n+1);
-	rep(i,1,n+1)
+	rep(i,1,n+1){
 		if(mp[c[i]].size()+1>mx) mx=mp[c[i]].size()+1,ans=c[i];
+	}
 	cout<<ans;
 }
 
