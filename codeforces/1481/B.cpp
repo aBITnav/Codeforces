a34ffdf66543e2313da38d5f6e41fce8713ca93d
Abhinav <abhinav98jha@gmail.com>
2021-02-13 19:36:00 +0530

    Add solution for problem `B - New Colony` Link: https://codeforces.com/contest/1481/problem/B Tags: brute force, greedy, implementation, *1100 Ref: https://codeforces.com/contest/1481/submission/107284383

---
+++ codeforces/1481/B.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int 		long long
+#define pb 			push_back
+#define all(a) 		a.begin(),a.end()
+#define pii 		pair<int,int>
+#define fi 			first
+#define se 			second
+#define rep(i,k,n) 	for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+const int M=1e9+7,N=205001;
+
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+
+
+void solve() {
+	int n,k,ans=-1;
+	cin>>n>>k;
+	int a[n+1];
+	rep(i,0,n) cin>>a[i];
+	a[n]=1e9;
+	rep(i,0,k){
+		int j=0;
+		while(a[j]>=a[j+1]) j++;
+		if(j==n-1){
+			ans=-2;break;}
+		a[j]++;
+		ans=j;
+		
+	}
+	cout<<ans+1<<"\n";
+}
+
+
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int t=1;
+	cin>>t;
+	while(t--) solve();
+	return 0;
+}
+
+
+
+
