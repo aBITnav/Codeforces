a0e48ed7916b3c0ebd3c3efad6914c7b2b1016b0
Abhinav <abhinav98jha@gmail.com>
2021-02-15 20:38:00 +0530

    Add solution for problem `B - Cat Cycle` Link: https://codeforces.com/contest/1487/problem/B Tags: math, number theory, *1200 Ref: https://codeforces.com/contest/1487/submission/107429111

---
+++ codeforces/1487/B.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int 		long long
+#define pb 			push_back
+#define all(a) 		a.begin(),a.end()
+#define pii 		pair<int,int>
+#define fi 			first
+#define se 			second
+#define rep(i,k,n) 	for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+const int M=1e9+7,N=205001;
+
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+
+
+void solve() {
+	int n,k,ans;
+	cin>>n>>k;
+	if(n%2==0) ans=(k)%n;
+	else {
+		int z=(k-1)/(n/2);
+		ans=(k+z)%n;
+	}
+	if(!ans) ans=n;
+	cout<<ans<<"\n";
+}
+
+
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int t=1;
+	cin>>t;
+	while(t--) solve();
+	return 0;
+}
+
+
+
+
