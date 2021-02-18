435eefc689705b049f42c668f6e7d5871f3a88a2
Abhinav <abhinav98jha@gmail.com>
2021-02-19 02:31:00 +0530

    Add solution for problem `C1 - Guessing the Greatest (easy version)` Link: https://codeforces.com/contest/1486/problem/C1 Tags: binary search, interactive Ref: https://codeforces.com/contest/1486/submission/107886359

--- codeforces/1486/C1.cpp
+++ codeforces/1486/C1.cpp
@@ -12,36 +12,29 @@ const int M=1e9+7,N=205001;
 int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
 int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
 
-map<pii,int> dp;
-
-int query(int l,int r){
-	if(dp.count({l,r})) return dp[{l,r}]; 
-	int x;
-	cout<<"? "<<l<<" "<<r<<endl;
-		cin>>x;
-	return dp[{l,r}]=x;
-}
 
 signed main(){
 ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 	int n,xlr,xlm,xmr;
 	cin>>n;
 	int l=1,r=n,m;
-	
 	while(1){
 		if(r==l) {
 			cout<<"! "<<l<<endl;
 			return 0;
 		}
 		if(r-l==1){
-			xlr=query(l,r);
+			cout<<"? "<<l<<" "<<r<<endl;
+			cin>>xlr;
 			cout<<"! "<<(l+r-xlr)<<endl;
 			return 0;	
 		}
 		m=(l+r)/2;
-		xlr=query(l,r);
+		cout<<"? "<<l<<" "<<r<<endl;
+		cin>>xlr;
 		if(xlr<=m){
-			xlm=query(l,m);
+			cout<<"? "<<l<<" "<<m<<endl;
+			cin>>xlm;
 			if(xlm==xlr) r=m;
 			else l=m+1;
 		}else{
@@ -49,7 +42,8 @@ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 				r=m;
 				continue;
 			}
-			xmr=query(m+1,r);
+			cout<<"? "<<m+1<<" "<<r<<endl;
+			cin>>xmr;
 			if(xmr==xlr) l=m+1;
 			else r=m;
 		}
