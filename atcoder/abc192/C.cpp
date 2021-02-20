dbd7aa8278b94eff8be237e80da6751abf707651
Abhinav <abhinav98jha@gmail.com>
2021-02-20 18:00:00 +0530

    Add solution for problem `C - Kaprekar Number` Link: https://atcoder.jp/contests/abc192/tasks/abc192_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc192/submissions/20307326

---
+++ atcoder/abc192/C.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int 		long long
+#define pb 			push_back
+#define all(a) 		a.begin(),a.end()
+#define pii 		pair<int,int>
+#define fi 			first
+#define se 			second
+#define rep(i,k,n) 	for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+const int M=1e9+7,N=205001;
+
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+string f(string n){
+	sort(all(n));
+	int a=stoll(n);
+	reverse(all(n));
+	int b=stoll(n);
+	return to_string(b-a);
+}
+
+void solve() {
+	string n;
+	int k;
+	cin>>n>>k;
+	rep(i,0,k)
+		n=f(n);
+	cout<<n;
+}
+
+
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int t=1;
+	//cin>>t;
+	while(t--) solve();
+	return 0;
+}
+
+
+
+
