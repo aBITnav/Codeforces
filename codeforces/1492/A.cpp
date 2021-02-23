4f6759e359df82e081a2cdeb4239f9905777b16a
Abhinav <abhinav98jha@gmail.com>
2021-02-23 14:48:00 +0530

    Add solution for problem `A - Three swimmers` Link: https://codeforces.com/contest/1492/problem/A Tags: math Ref: https://codeforces.com/contest/1492/submission/108247336

---
+++ codeforces/1492/A.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int 		long long
+#define pb 			push_back
+#define all(a) 		a.begin(),a.end()
+#define pii 		pair<int,int>
+#define fi 			first
+#define se 			second
+#define rep(i,k,n) 	for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+const int M=1e9+7,N=205001;
+
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+
+
+void solve() {
+	int p,ans=2e18,x,d;
+	cin>>p;
+	rep(i,0,3){
+		cin>>x;
+		d=p/x;
+		if(p%x) d++;
+		ans=min(ans,d*x-p);
+	} 
+	cout<<ans<<"\n";
+}
+
+
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int t=1;
+	cin>>t;
+	while(t--) solve();
+	return 0;
+}
+
+
+
+
