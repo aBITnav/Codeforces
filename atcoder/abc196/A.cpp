3d1e5a55c0054c59163780b693c7a850c3925998
Abhinav <abhinav98jha@gmail.com>
2021-03-20 17:33:00 +0530

    Add solution for problem `A - Difference Max` Link: https://atcoder.jp/contests/abc196/tasks/abc196_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc196/submissions/21066953

---
+++ atcoder/abc196/A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int 		long long
+#define pb 			push_back
+#define all(a) 		a.begin(),a.end()
+#define pii 		pair<int,int>
+#define fi 			first
+#define se 			second
+#define rep(i,k,n) 	for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+const int M=1e9+7,N=205001;
+
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+
+
+void solve() {
+	int a,b,c,d,mx=-1e9;
+	cin>>a>>b>>c>>d;
+	rep(x,a,b+1) rep(y,c,d+1) mx=max(mx,x-y);
+	cout<<mx; 
+
+}
+
+
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int t=1;
+	//cin>>t;
+	while(t--) solve();
+	return 0;
+}
+
+
+
+
