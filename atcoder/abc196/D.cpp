84eade698d93fbf0159cfe76f509a7b19619f39a
Abhinav <abhinav98jha@gmail.com>
2021-03-22 21:58:00 +0530

    Add solution for problem `D - Hanjo` Link: https://atcoder.jp/contests/abc196/tasks/abc196_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc196/submissions/21182696

--- atcoder/abc196/D.cpp
+++ atcoder/abc196/D.cpp
@@ -11,15 +11,19 @@ void solve(int m,int a,int b) {
 		ans+=(a==0 and b==0);
 		return;
 	}
+	bool ok=0;
 	rep(i,0,h) rep(j,0,w) {
 		int p=(1<<(i*w+j));
 		if(!(m&p)){
+			ok=1;
 			m|=p;
-			if(b>0) solve(m|p,a,b-1);
+			if(b>0) 
+				solve(m,a,b-1);
 			if(a>0 and j!=w-1 and !(m&(p<<1)))
-				solve(m|p|(p<<1),a-1,b);
+				solve(m|(p<<1),a-1,b);
 			if(a>0 and i!=h-1 and !(m&(p<<w)))
-				solve(m|p|(p<<w),a-1,b);
+				solve(m|(p<<w),a-1,b);
+			break;
 		}
 	}
 }
