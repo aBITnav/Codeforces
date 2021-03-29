4c53b725721be8bee685cda3c0c2edf4aec04ffc
Abhinav <abhinav98jha@gmail.com>
2021-03-29 20:11:00 +0530

    Add solution for problem `A - GCD Sum` Link: https://codeforces.com/contest/1498/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/1498/submission/111359313

---
+++ codeforces/1498/A.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int 		long long
+#define pb 			push_back
+#define all(a) 		a.begin(),a.end()
+#define pii 		pair<int,int>
+#define fi 			first
+#define se 			second
+#define rep(i,k,n) 	for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+const int M=1e9+7,N=205001;
+
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+int sd(int n){
+	int s=0;
+	while(n){
+		s+=n%10;
+		n/=10;
+	}
+	return s;
+}
+
+void solve() {
+	int n;
+	cin>>n;
+	while(__gcd(n,sd(n))==1) n++;
+	cout<<n<<"\n";
+}
+
+
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int t=1;
+	cin>>t;
+	while(t--) solve();
+	return 0;
+}
+
+
+
+
