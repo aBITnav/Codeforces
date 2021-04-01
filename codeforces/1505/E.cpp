74890e9392d778de279317fae59d807980a67a1a
Abhinav <abhinav98jha@gmail.com>
2021-04-01 21:26:00 +0530

    Add solution for problem `E - Cakewalk` Link: https://codeforces.com/contest/1505/problem/E Tags: greedy, implementation, shortest paths Ref: https://codeforces.com/contest/1505/submission/111710224

---
+++ codeforces/1505/E.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int 		long long
+#define pb 			push_back
+#define all(a) 		a.begin(),a.end()
+#define pii 		pair<int,int>
+#define fi 			first
+#define se 			second
+#define rep(i,k,n) 	for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+const int M=1e9+7,N=205001;
+
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+int sod(int n){
+	int ans=0;
+	while(n){
+		ans+=n%10;
+		n/=10;
+	}
+	return ans;
+}
+
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int m,n;
+	char c;
+	vector<pair<int,pii>> v;
+	cin>>m>>n;
+	rep(i,0,m) rep(j,0,n){
+		cin>>c;
+		if(c=='*') v.pb({i+j,{i,j}});
+	}
+	n=v.size();
+	if(n<2){
+		cout<<n;
+		return 0;
+	}
+	sort(all(v));
+	int i=0,ans=1,j;
+	
+	while(i<n-1){
+		for(j=i+1;j<n;j++) 
+			if(v[j].se.fi>=v[i].se.fi and v[j].se.se>=v[i].se.se){
+				ans++;
+				i=j;
+				break;
+			}
+		if(j==n) break;
+	}
+	cout<<ans;
+}
+
+
+
+
