bf21abd1c2b176420c332ccf63bf6307c7de8fff
Abhinav <abhinav98jha@gmail.com>
2021-05-01 17:56:00 +0530

    Add solution for problem `B - Sign of Friendship` Link: https://atcoder.jp/contests/zone2021/tasks/zone2021_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/zone2021/submissions/22209241

---
+++ atcoder/zone2021/B.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int 		long long
+#define pb 			push_back
+#define all(a) 		a.begin(),a.end()
+#define pii 		pair<int,int>
+#define fi 			first
+#define se 			second
+#define rep(i,k,n) 	for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+const int M=1e9+7,N=205001;
+
+int mm(int x,int y){x%=M,y%=M;return (x*y)%M;}//Modular Multiply
+int po(int x,int y){ if(!y)return 1;int a=po(x,y/2)%M;if(y%2)return mm(a,mm(a,x));return mm(a,a);}//(x**y)%M
+
+
+
+void solve() {
+	int n;
+	double x1,y1,x2,y2,m,mx=1e9;
+	cin>>n>>x1>>y1;
+	while(n--){
+		cin>>x2>>y2;
+		m=(y2-y1)/(x2-x1);
+		mx=min(mx,m);
+	}
+	m=y1-mx*x1;
+	m=max(m,0.0);
+	printf("%0.9lf",m);
+}
+
+
+signed main(){
+ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int t=1;
+	//cin>>t;
+	while(t--) solve();
+	return 0;
+}
+
+
+
+
